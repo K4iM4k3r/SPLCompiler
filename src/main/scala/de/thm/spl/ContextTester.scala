49cc3d9e4ff27945ac814b354f28988fcaa33000
Kai Schäfer <kai.schaefer@mni.thm.de>
2018-05-17 09:47:33 +0200

    add Show Ast Method

--- src/main/scala/de/thm/spl/ContextTester.scala
+++ src/main/scala/de/thm/spl/ContextTester.scala
@@ -3,10 +3,13 @@ package de.thm.spl
 import de.thm.MessageLogger
 import de.thm.spl.Parser.SPLParser
 import de.thm.spl.syntax.Definitions._
+import de.thm.spl.syntax.Statement
+import de.thm.spl.syntax.Statements.{CompoundStatement, If, While}
 
 import scala.io.Source
 
 object ContextTester{
+   final val SPACE: String = "     "
 
     def main(args: Array[String]): Unit = {
 //      val files =  List("acker.spl", "arrayasgn.spl", "arrayparm.spl", "bits.spl", "queens.spl", "gcd.spl", "multiply.spl", "partind1.spl", "partind2.spl", "reftest.spl", "sierpinski.spl", "swap.spl", "" +
@@ -14,7 +17,7 @@ object ContextTester{
 //
 //      files.foreach(testContext)
 
-      testContext("partind1.spl")
+      testContext("queens.spl")
     }
 
 
@@ -27,7 +30,8 @@ object ContextTester{
 
       MessageLogger.clear()
       val ast = SPLParser(code)
-      println(ast)
+//      println(ast)
+      printAst(ast.get)
       println(MessageLogger)
 
       if (!MessageLogger.hasErrorHappened) {
@@ -39,31 +43,35 @@ object ContextTester{
     }
 
 
+  def printAst(ast: syntax.Program): Unit ={
+    println("Program( ")
+    println(ast.definitions.map{
+      case ProcedureDefinition(name, params, vars, body) =>
+        var proc: String = SPACE + "ProcedureDefinition(" + name + ",\n"
 
+        if(params.nonEmpty) proc += SPACE * 2 + params.mkString(",\n"+SPACE * 2) + ",\n"
+        if(vars.nonEmpty)   proc += SPACE * 2 + vars.mkString(",\n"+SPACE * 2) + ",\n"
 
+        proc += body.map(f => styleStmt(3, f) ).mkString(",\n")
+        proc += "\n"+ SPACE +")"
+        proc
+      case d => SPACE + d.toString
+    }.mkString(", \n"))
+  }
 
+  def styleStmt(deepth: Int, statement: Statement): String = {
+    val dSPACE: String = SPACE * deepth
+    statement match {
+      case While(condition, body) =>
+        dSPACE + "While(" + condition.toString + ",\n" + styleStmt(deepth + 1, body) + "\n" + dSPACE + ")"
+      case If(condition, thenStatements, elseStatements) =>
+        dSPACE + "If(" + condition.toString + ",\n" + styleStmt(deepth + 1, thenStatements) + "\n" + dSPACE + ",\n" + styleStmt(deepth + 1, elseStatements) + "\n" + dSPACE + ")"
+      case CompoundStatement(body) =>
+        dSPACE + "CompoundStatement(\n" +  body.map(f => styleStmt(deepth + 1, f) ).mkString(",\n") + "\n" + dSPACE + ")"
+      case stmt =>
+        dSPACE + stmt.toString
 
-
-
-
-
-
-
-
-
-
-
-
-
-//    val source  = Source.fromFile("./parser/queens.spl")
-//    var code    = try source.mkString finally source.close()
-//
-////    println(code)
-//
-//
-//    }
-//
-//
-
+    }
+  }
 
 }
